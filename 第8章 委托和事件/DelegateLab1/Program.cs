da72912772ff73cc2f539e982913e66cb4d5d52f
frank <s055523@gmail.com>
2018-09-29 10:58:18 +1200

    code structure fix

---
+++ 第8章 委托和事件/DelegateLab1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DelegateLab
+{
+    public class TestClass
+    {
+        //静态方法
+        public static void CallBackStatic(int i)
+        {
+            Console.WriteLine("回调静态方法:" + i);
+        }
+
+        //实例方法
+        public void CallBackInstance(int i)
+        {
+            Console.WriteLine("回调实例方法:" + i);
+        }
+    }
+
+    class Program
+    {
+        //定义一个委托
+        public delegate void CallBack(int i);
+
+        static void Main(string[] args)
+        {
+            var random = RandomNumber();
+            Console.WriteLine("当运行完random函数之后，运行回调函数并传入random函数的结果");
+
+            var info = typeof(TestClass).GetMethod("CallBackStatic");
+
+            //回调静态函数不需要实例
+            //通过反射传入一个方法的MethodInfo
+            var cb = (CallBack)Delegate.CreateDelegate(typeof(CallBack), null, info);
+
+            //传递一组可选的参数并使用DynamicInvoke调用
+            cb.DynamicInvoke(random);
+
+            random = RandomNumber();
+            Console.WriteLine("当运行完random函数之后，运行回调函数并传入random函数的结果");
+
+            info = typeof(TestClass).GetMethod("CallBackInstance");
+            var p = new TestClass();
+
+            //回调实例函数需要实例 
+            cb = (CallBack)Delegate.CreateDelegate(typeof(CallBack), p, info);
+            cb.DynamicInvoke(random);
+
+            Console.ReadKey();
+        }
+
+        private static int RandomNumber()
+        {
+            var random = new Random();
+            return random.Next(0, 100);
+        }
+    }
+
+}
---
+++ 第9章 泛型/GenericLab1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericLab
+{
+    //在object之下，泛型类型之上的一个中间过渡类型
+    //如果省去它，则一个new List<TypedMember<T>>仍然只能包含一种类型
+    class Member
+    {
+    }
+
+    class TypedMember<T> : Member
+    {
+        private T data;
+        public TypedMember(T d)
+        {
+            data = d;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tom = new { Name = "Tom", Age = 15 };
+            Console.WriteLine("{0}: {1}", tom.Name, tom.Age);
+
+            Console.WriteLine(tom.GetType());
+
+            Console.ReadKey();
+        }
+
+        static int Compare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+
+        //泛型方法，输入参数类型必须相同
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+            Console.WriteLine("Swap<T>");
+        }
+
+        //输入参数必须是两个int的非泛型方法
+        static void Swap(ref int a, ref int b)
+        {
+            Console.WriteLine("swap");
+        }
+
+        //泛型方法，第一个输入参数任意，第二个必须为int
+        static void Swap<T>(ref T lhs, ref int rhs)
+        {
+            Console.WriteLine("swap2");
+        }
+
+        //泛型方法，输入参数类型任意，因此，如果两个参数类型相同时，将和第一个方法完全相同
+        //使用两个类型参数不意味着它们必须是不同的
+        static void Swap<T,K>(ref T lhs, ref K rhs)
+        {
+            Console.WriteLine("Swap<T,K>");
+        }
+    }
+}
